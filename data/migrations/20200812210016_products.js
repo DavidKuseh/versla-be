157af8975ece09eb513e67a2e98b8c8edc6c0477
david kuseh <davegru203@gmail.com>
2020-08-12 23:04:28 +0100

    add product migration

--- data/migrations/20200812210016_products.js
+++ data/migrations/20200812210016_products.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('products', table => {
+      table
+          .increments();
+      table
+          .string('name', 256)
+          .notNullable();
+      table
+          .text('description', longtext)
+          .notNullable();
+      table
+          .integer('price')
+          .notNullable();
+      table
+          .string('condition')
+          .notNullable()
+      table
+          .string('category')
+          .notNullable();
+    })
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex) {
+    
+  };
   
-};
